d79c25c244e481c5a23ba11c247f0863edb65a1e
Amir Yue Zhou <amirzhou001@gmail.com>
2024-06-17 18:23:43 -0600

    optional interface properties

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,11 @@
 import { User } from './models/User';
 
 const user1 = new User({ name: 'Amir', age: 28 });
-const age = user1.get('age');
+let age = user1.get('age');
 console.log(age);
+
+user1.set({ name: 'Amir Zhou', age: 34 });
+age = user1.get('age');
+console.log(age);
+
+user1.set({ age: 40 });
