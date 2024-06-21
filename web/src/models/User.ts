b4455aba3d21f7383c1b5054a8e03f2f496f8dd2
Amir Yue Zhou <amirzhou001@gmail.com>
2024-06-21 11:14:45 -0600

    extract the Attributes class with major issue
    one thing need to fix is if we go generic with
    this class, TS wouldn't know the return type with
    get()

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse, AxiosError } from 'axios';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
   id?: number;
@@ -11,14 +12,9 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3080/users';
 
 export class User {
+  attributes: Attributes<UserProps> = new Attributes<UserProps>({});
   events: Eventing = new Eventing();
   sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  constructor(public data: UserProps) {}
 
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+  constructor(data: UserProps) {}
 }
