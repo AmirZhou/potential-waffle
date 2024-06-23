e9bfbfe922cca5b791c38ce4f0dd2637600c03b7
AmirZhou <amirzhou001@gmail.com>
2024-06-22 18:56:05 -0600

    starting the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('test1');
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3099;
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi there!</h1>
+    </div>
+    `);
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
