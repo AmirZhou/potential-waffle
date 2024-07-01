cd25844434acf0d622b4a2f2d48c13da5ff9acb8
Amir Yue Zhou <amirzhou001@gmail.com>
2024-07-01 16:54:52 -0600

    wrap method with decorator

--- features/decoraters.ts
+++ features/decoraters.ts
@@ -14,10 +14,18 @@ class Boat {
 
 // arrow function not work
 function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  console.log(`target: ${target}`);
-  console.log(`key: ${key}`);
+  const method = desc.value;
+  desc.value = () => {
+    try {
+      method();
+    } catch (err) {
+      console.log('boat sank');
+    }
+  };
 }
 
+const aBoat = new Boat();
+aBoat.pilot();
 // decorator is just syntax suguar, we could even do it manully like the following
 // which will yield the same result
 // It's just a function tht receive the prototype and key
