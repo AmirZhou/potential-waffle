ace77b19902afbc7fbc6e9a1efdcdc75af887fe5
Amir Yue Zhou <amirzhou001@gmail.com>
2024-07-02 10:53:36 -0600

    use decorator factory
    decorator factory receive custom info that been
    added to the property (prototype) with Refect

---
+++ features/metadata2.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+
+class Plane {
+  color: string = 'red';
+
+  @marker
+  fly(): void {
+    console.log('vrrrrrrrr');
+  }
+
+  @markerFactory('note', 'crashing')
+  status(): void {
+    console.log('looks good');
+  }
+}
+
+function marker(target: Plane, key: string, desc: PropertyDescriptor) {
+  Reflect.defineMetadata('secret', 'Hijacted', target, key);
+}
+
+function markerFactory(note: string, message: string) {
+  return function (target: Plane, key: string, desc: PropertyDescriptor) {
+    Reflect.defineMetadata(note, message, target, key);
+  };
+}
+
+const planeTest = new Plane();
+
+const secret = Reflect.getMetadata('secret', planeTest, 'fly');
+console.log(secret);
+const secret2 = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+console.log(secret2);
+
+const noteOnInstance = Reflect.getMetadata('note', planeTest, 'status');
+console.log(noteOnInstance);
+const noteUndefined = Reflect.getMetadata('note', Plane, 'status');
+console.log(noteUndefined);
+const note = Reflect.getMetadata('note', Plane.prototype, 'status');
+console.log(note);
